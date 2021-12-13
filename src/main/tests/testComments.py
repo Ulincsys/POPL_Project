e20d75fa73169a717137f4002ec2ae29bd3aa689
Sean Brennan <slbvp6@mail.missouri.edu>
2021-12-12 18:31:03 -0600

    Added tests

---
+++ src/main/tests/testComments.py
@@ -0,0 +1,4 @@
+# Here is a simple python file to test comments
+variable = -23 # Here is a simple statement assigning a variable to a signed integer
+variable2 = 23
+# This line and the first should be ignored, and the comment after the statement on line 2 as well
