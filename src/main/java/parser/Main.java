bbadcce9d837d61b394eea8f509a0e182e527a03
Sean Lee Brennan <brennansean6@gmail.com>
2021-12-14 16:08:37 -0600

    Started looking into visualizations included w/ ANTLR

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.gui.TestRig;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -20,5 +21,19 @@ public class Main {
         Python3Parser parser = new Python3Parser(new CommonTokenStream(lexer));
         ParseTree tree = parser.parse();
         System.out.println(tree.toString());
+
+        //Visualizations: grun/TestRig included w/ antlr
+        //Source: https://www.antlr.org/api/JavaTool/org/antlr/v4/gui/TestRig.html#TestRig(java.lang.String[])
+        try{
+            String grammarFileName = "Python3";
+            String startRuleName = " r ";
+            String flags = "-gui";
+            String[] params = {grammarFileName, startRuleName, flags};
+            TestRig viz = new TestRig(params); //Look into this
+
+        }
+        catch(Exception e) {
+            System.out.println("Exception thrown for visualizations: " + e);
+        }
     }
 }
