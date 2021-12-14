aee5b7d54a4b7e765150c57d09ff69eabefcc8aa
ulincsys <28362836a@gmail.com>
2021-12-14 17:21:38 -0600

    Modified input to use Scanner to detect EOF

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -3,6 +3,9 @@ import java.nio.CharBuffer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import java.util.Scanner;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -10,14 +13,17 @@ import org.antlr.v4.gui.TestRig;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        char[] buf = new char[1024];
-        int chars_read = 0;
-        InputStreamReader reader = new InputStreamReader(System.in);
-        int temp;
-        while ((temp = reader.read(buf, chars_read, 1024-chars_read)) > 0) chars_read += temp;
-        System.out.println("Chars read: " + chars_read);
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\Z");
+        // char[] buf = new char[1024];
+        // int chars_read = 0;
+        // InputStreamReader reader = new InputStreamReader(System.in);
+        // int temp;
+        // while ((temp = reader.read(buf, chars_read, 1024-chars_read)) > 0) chars_read += temp;
+        String input = scanner.next();
+        System.out.format("Chars read: %d\n", input.length());
 //        CalculatorLexer lexer = new CalculatorLexer(CharStreams.fromString("3 * 4 + 5"));
-        Python3Lexer lexer = new Python3Lexer(CharStreams.fromString(new String(buf, 0, chars_read)));
+        Python3Lexer lexer = new Python3Lexer(CharStreams.fromString(input));
         Python3Parser parser = new Python3Parser(new CommonTokenStream(lexer));
         ParseTree tree = parser.parse();
         System.out.println(tree.toString());
